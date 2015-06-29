77177eac21a4c7cbc42bf09452fa889b41bbd198
Mover <moverinfo@gmail.com>
2015-06-29 22:15:11 +0800

    Update smalltalk.java

--- src/tinystruct/examples/smalltalk.java
+++ src/tinystruct/examples/smalltalk.java
@@ -1,7 +1,9 @@
 package tinystruct.examples;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,19 +12,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.tinystruct.AbstractApplication;
 import org.tinystruct.ApplicationException;
+import org.tinystruct.data.component.Builder;
 import org.tinystruct.system.util.StringUtilities;
 
 public class smalltalk extends AbstractApplication {
 
-	private final Map<String, String> map = Collections.synchronizedMap(new HashMap<String, String>());
+	private final Map<String, Object> map = Collections.synchronizedMap(new HashMap<String, Object>());
 	
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
 		this.setAction("talk", "index");
 		this.setAction("talk/start", "start");
 		this.setAction("talk/update", "update");
 		this.setAction("talk/save", "save");
+		this.setAction("talk/command", "command");
 		this.setAction("talk/version", "version");
 	}
 	
@@ -39,40 +42,73 @@ public class smalltalk extends AbstractApplication {
 
 	public void update() throws ApplicationException {
 		HttpServletResponse response = (HttpServletResponse) this.context.getAttribute("HTTP_RESPONSE");
-		
+
 		synchronized(this.map) {
 			try {
 				this.map.wait();
 			} catch (InterruptedException e) {
 				throw new ApplicationException(e.getMessage(),e);
 			}
+			
 			if(this.map.containsKey("textvalue")) {
 				System.out.println(this.getVariable("browser").getValue().toString()+":"+this.map.get("textvalue"));
 				try {
-					response.getWriter().println(new StringUtilities(this.map.get("textvalue")).replace('\n', ""));
+					response.getWriter().println(new StringUtilities(this.map.get("textvalue").toString().trim()).replace('\n', ""));
 					response.getWriter().flush();
 					response.getWriter().close();
 				} catch (IOException e) {
 					throw new ApplicationException(e.getMessage(),e);
 				}
-
+			}
+			
+			if(this.map.containsKey("command")) {
+				System.out.println(this.map.get("command"));
+				try {
+					response.getWriter().println(new StringUtilities(this.map.get("command").toString().trim()).replace('\n', ""));
+					response.getWriter().flush();
+					response.getWriter().close();
+				} catch (IOException e) {
+					throw new ApplicationException(e.getMessage(),e);
+				}
 			}
 		}
 	}
 
 	public boolean save() {
 		HttpServletRequest request = (HttpServletRequest) this.context.getAttribute("HTTP_REQUEST");
+		SimpleDateFormat format = new SimpleDateFormat("Y-M-d h:m:s");
 		
 		synchronized(this.map){
 			String[] agent = request.getHeader("User-Agent").split(" ");
 			this.setVariable("browser", agent[agent.length-1]);
-			
 			if(this.map.containsKey("textvalue")) this.map.remove("textvalue");
-			this.map.put("textvalue", request.getSession().getAttribute("user") + ":" + request.getParameter("text"));
+			
+			Builder builder = new Builder();
+			builder.put("user", request.getSession().getAttribute("user"));
+			builder.put("time", format.format(new Date()));
+			builder.put("message", request.getParameter("text"));
+			
+			this.map.put("textvalue", builder);
 			this.map.notifyAll();
 			return true;
 		}
-
+	}
+	
+	public boolean command() {
+		HttpServletRequest request = (HttpServletRequest) this.context.getAttribute("HTTP_REQUEST");
+		
+		synchronized(this.map){
+			if(this.map.containsKey("command")) this.map.remove("command");
+			if(this.map.containsKey("textvalue")) this.map.remove("textvalue");
+			
+			Builder builder = new Builder();
+			builder.put("user", request.getSession().getAttribute("user"));
+			builder.put("cmd", request.getParameter("cmd"));
+			
+			this.map.put("command", builder);
+			this.map.notifyAll();
+			return true;
+		}
 	}
 
 	@Override
