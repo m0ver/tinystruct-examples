be2d6024968f82f4f855d5675e3336c69459e730
Mover <moverinfo@gmail.com>
2016-05-09 16:38:38 +0800

    Update smalltalk.java

--- src/tinystruct/examples/smalltalk.java
+++ src/tinystruct/examples/smalltalk.java
@@ -1,11 +1,12 @@
 package tinystruct.examples;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -13,6 +14,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -32,280 +34,284 @@ import org.tinystruct.transfer.http.upload.MultipartFormData;
 
 public class smalltalk extends AbstractApplication {
 
-	private final Map<String, Queue<Builder>> map = Collections
-	    .synchronizedMap(new HashMap<String, Queue<Builder>>());
-	private Queue<Builder> list;
+  private final Map<String, Queue<Builder>> map = Collections
+      .synchronizedMap(new HashMap<String, Queue<Builder>>());
+  private Queue<Builder> list;
 
-	@Override
-	public void init() {
-		this.setAction("talk", "index");
-		this.setAction("talk/join", "join");
-		this.setAction("talk/start", "start");
-		this.setAction("talk/update", "update");
-		this.setAction("talk/save", "save");
-		this.setAction("talk/upload", "upload");
-		this.setAction("talk/command", "command");
-		this.setAction("talk/topic", "topic");
-		this.setAction("talk/matrix", "matrix");
-		this.setAction("talk/version", "version");
+  @Override
+  public void init() {
+    this.setAction("talk", "index");
+    this.setAction("talk/join", "join");
+    this.setAction("talk/start", "start");
+    this.setAction("talk/update", "update");
+    this.setAction("talk/save", "save");
+    this.setAction("talk/upload", "upload");
+    this.setAction("talk/command", "command");
+    this.setAction("talk/topic", "topic");
+    this.setAction("talk/matrix", "matrix");
+    this.setAction("talk/version", "version");
 
-		this.setVariable("message", "");
-		this.setVariable("topic", "");
-	}
+    this.setVariable("message", "");
+    this.setVariable("topic", "");
+  }
 
-	public smalltalk index() {
-		HttpServletRequest request = (HttpServletRequest) this.context
-		    .getAttribute("HTTP_REQUEST");
-		Object code = request.getSession().getAttribute("meeting_code");
+  public smalltalk index() {
+    HttpServletRequest request = (HttpServletRequest) this.context
+        .getAttribute("HTTP_REQUEST");
+    Object code = request.getSession().getAttribute("meeting_code");
 
-		if (code == null) {
-			String key = java.util.UUID.randomUUID().toString();
-			request.getSession(true).setAttribute("meeting_code", key);
+    if (code == null) {
+      String key = java.util.UUID.randomUUID().toString();
+      request.getSession(true).setAttribute("meeting_code", key);
 
-			this.list = new ConcurrentLinkedQueue<Builder>();
-			this.map.put(key, this.list);
-			this.setVariable("meeting_code", key);
+      this.list = new PriorityQueue<Builder>();
+      this.map.put(key, this.list);
+      this.setVariable("meeting_code", key);
 
-			System.out.println("New meeting generated:" + key);
-		} else {
-			this.setVariable("meeting_code", code.toString());
-			if (this.getVariable(code.toString()) != null) {
-				this.setVariable("topic", this.getVariable(code.toString()).getValue()
-				    .toString().replaceAll("[\r\n]", "<br />"), true);
-			}
-		}
+      System.out.println("New meeting generated:" + key);
+    } else {
+      this.setVariable("meeting_code", code.toString());
+      if (this.getVariable(code.toString()) != null) {
+        this.setVariable("topic", this.getVariable(code.toString()).getValue()
+            .toString().replaceAll("[\r\n]", "<br />"), true);
+      }
+    }
 
-		return this;
-	}
+    return this;
+  }
 
-	public String matrix() throws ApplicationException {
-		HttpServletRequest request = (HttpServletRequest) this.context
-		    .getAttribute("HTTP_REQUEST");
+  public String matrix() throws ApplicationException {
+    HttpServletRequest request = (HttpServletRequest) this.context
+        .getAttribute("HTTP_REQUEST");
 
-		if (request.getParameter("meeting_code") != null) {
-			BufferedImage qrImage = Matrix.toQRImage(this.getLink("talk/join") + "/"
-			    + request.getParameter("meeting_code"), 100, 100);
+    if (request.getParameter("meeting_code") != null) {
+      BufferedImage qrImage = Matrix.toQRImage(this.getLink("talk/join") + "/"
+          + request.getParameter("meeting_code"), 100, 100);
 
-			return "data:image/png;base64," + Matrix.getBase64Image(qrImage);
-		}
+      return "data:image/png;base64," + Matrix.getBase64Image(qrImage);
+    }
 
-		return "";
-	}
+    return "";
+  }
 
-	public String join(String meeting_code) throws ApplicationException {
+  public String join(String meeting_code) throws ApplicationException {
 
-		if (map.containsKey(meeting_code)) {
-			HttpServletRequest request = (HttpServletRequest) this.context
-			    .getAttribute("HTTP_REQUEST");
-			HttpServletResponse response = (HttpServletResponse) this.context
-			    .getAttribute("HTTP_RESPONSE");
+    if (map.containsKey(meeting_code)) {
+      HttpServletRequest request = (HttpServletRequest) this.context
+          .getAttribute("HTTP_REQUEST");
+      HttpServletResponse response = (HttpServletResponse) this.context
+          .getAttribute("HTTP_RESPONSE");
 
-			request.getSession(true).setAttribute("meeting_code", meeting_code);
+      request.getSession(true).setAttribute("meeting_code", meeting_code);
 
-			this.setVariable("meeting_code", meeting_code);
+      this.setVariable("meeting_code", meeting_code);
 
-			Reforward reforward = new Reforward(request, response);
-			reforward.setDefault("/?q=talk");
-			reforward.forward();
-		} else {
-			return "Invalid meeting code.";
-		}
+      Reforward reforward = new Reforward(request, response);
+      reforward.setDefault("/?q=talk");
+      reforward.forward();
+    } else {
+      return "Invalid meeting code.";
+    }
 
-		return "Please start the conversation with your name: "
-		    + this.config.get("default.base_url") + "talk/start/YOUR NAME";
-	}
+    return "Please start the conversation with your name: "
+        + this.config.get("default.base_url") + "talk/start/YOUR NAME";
+  }
 
-	public String start(String name) throws ApplicationException {
-		HttpServletRequest request = (HttpServletRequest) this.context
-		    .getAttribute("HTTP_REQUEST");
-		HttpServletResponse response = (HttpServletResponse) this.context
-		    .getAttribute("HTTP_RESPONSE");
+  public String start(String name) throws ApplicationException {
+    HttpServletRequest request = (HttpServletRequest) this.context
+        .getAttribute("HTTP_REQUEST");
+    HttpServletResponse response = (HttpServletResponse) this.context
+        .getAttribute("HTTP_RESPONSE");
 
-		Object meeting_code = request.getSession().getAttribute("meeting_code");
-		if (meeting_code == null) {
-			Reforward reforward = new Reforward(request, response);
-			reforward.setDefault("/?q=talk");
-			reforward.forward();
-		} else {
-			this.setVariable("meeting_code", meeting_code.toString());
-		}
-		request.getSession().setAttribute("user", name);
+    Object meeting_code = request.getSession().getAttribute("meeting_code");
+    if (meeting_code == null) {
+      Reforward reforward = new Reforward(request, response);
+      reforward.setDefault("/?q=talk");
+      reforward.forward();
+    } else {
+      this.setVariable("meeting_code", meeting_code.toString());
+    }
+    request.getSession().setAttribute("user", name);
 
-		return name;
-	}
+    return name;
+  }
 
-	public String update() throws ApplicationException {
-		HttpServletRequest request = (HttpServletRequest) this.context
-		    .getAttribute("HTTP_REQUEST");
-		if (request.getSession().getAttribute("meeting_code") != null) {
-			this.checkup(request);
-			synchronized (this.list) {
-				try {
-					this.list.wait();
-				} catch (InterruptedException e) {
-					throw new ApplicationException(e.getMessage(), e);
-				}
+  public String update() throws ApplicationException {
+    HttpServletRequest request = (HttpServletRequest) this.context
+        .getAttribute("HTTP_REQUEST");
+    if (request.getSession().getAttribute("meeting_code") != null) {
+      this.checkup(request);
+      String message;
+      synchronized (this.list) {
+        try {
+          this.list.wait();
+        } catch (InterruptedException e) {
+          throw new ApplicationException(e.getMessage(), e);
+        }
 
-				if (!this.list.isEmpty()) {
-					String message = this.list.peek().toString();
-					System.out.println("["
-					    + request.getSession(true).getAttribute("meeting_code") + "]:"
-					    + message);
-					return message.trim();
-				}
-			}
-		}
+        if (!this.list.isEmpty()) {
+          message = this.list.peek().toString();
+          System.out.println("["
+              + request.getSession(true).getAttribute("meeting_code") + "]:"
+              + message);
+          return message.trim();
+        }
+      }
+    }
 
-		return "";
-	}
+    return "";
+  }
 
-	public boolean save() {
-		HttpServletRequest request = (HttpServletRequest) this.context
-		    .getAttribute("HTTP_REQUEST");
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-M-d h:m:s");
-		if (request.getSession().getAttribute("meeting_code") != null) {
-			if (!request.getParameter("text").isEmpty()) {
-				this.checkup(request);
-				synchronized (this.list) {
-					String[] agent = request.getHeader("User-Agent").split(" ");
-					this.setVariable("browser", agent[agent.length - 1]);
+  public boolean save() {
+    HttpServletRequest request = (HttpServletRequest) this.context
+        .getAttribute("HTTP_REQUEST");
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-M-d h:m:s");
+    if (request.getSession().getAttribute("meeting_code") != null) {
+      if (!request.getParameter("text").isEmpty()) {
+        this.checkup(request);
+        synchronized (this.list) {
+          String[] agent = request.getHeader("User-Agent").split(" ");
+          this.setVariable("browser", agent[agent.length - 1]);
 
-					Builder builder = this.list.poll();
-					if (builder == null) {
-						builder = new Builder();
-					} else {
-						builder.remove("cmd");
-					}
-
-					builder.put("user", request.getSession(true).getAttribute("user"));
-					builder.put("time", format.format(new Date()));
-					builder.put("message", filter(request.getParameter("text")));
-
-					this.list.add(builder);
-					this.list.notifyAll();
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	public String command() {
-		HttpServletRequest request = (HttpServletRequest) this.context
-		    .getAttribute("HTTP_REQUEST");
-		HttpServletResponse response = (HttpServletResponse) this.context
-		    .getAttribute("HTTP_RESPONSE");
-		
-		if (request.getSession().getAttribute("meeting_code") != null) {
-			response.setContentType("application/json");
-			if (request.getSession().getAttribute("user") == null) {
-				return "{ \"error\": \"missing user\" }";
-			}
-			
-			this.checkup(request);
-			synchronized (this.list) {
-				Builder builder = this.list.poll();
-				if (builder == null) {
-					builder = new Builder();
-				} else {
-					builder.remove("message");
-					builder.remove("time");
-				}
-
-				builder.put("user", request.getSession(true).getAttribute("user"));
-				builder.put("cmd", request.getParameter("cmd"));
-
-				this.list.add(builder);
-				this.list.notifyAll();
-				return "{}";
-			}
-		}
-
-		return "{ \"error\": \"expired\" }";
-	}
-
-	public boolean topic() {
-		HttpServletRequest request = (HttpServletRequest) this.context.getAttribute("HTTP_REQUEST");
-		if (request.getSession().getAttribute("meeting_code") != null) {
-			String key = request.getSession().getAttribute("meeting_code").toString();
-			this.setVariable(key, filter(request.getParameter("topic")));
-			return true;
-		}
-
-		return false;
-	}
-
-	public smalltalk exit(String meeting_code) {
-		HttpServletRequest request = (HttpServletRequest) this.context.getAttribute("HTTP_REQUEST");
-		request.getSession().removeAttribute("meeting_code");
-
-		return this;
-	}
-
-	private void checkup(HttpServletRequest request) {
-		String key = request.getSession().getAttribute("meeting_code").toString();
-		if ((this.list = map.get(key)) == null) {
-			this.list = new ConcurrentLinkedQueue<Builder>();
-			this.map.put(key, this.list);
-			this.setVariable("meeting_code", key);
-		}
-	}
-
-	public String upload() throws ApplicationException {
-		HttpServletRequest request = (HttpServletRequest) this.context.getAttribute("HTTP_REQUEST");
-		HttpServletResponse response = (HttpServletResponse) this.context.getAttribute("HTTP_RESPONSE");
-		response.setContentType("text/html;charset=UTF-8");
-
-		// Create path components to save the file
-		final String path = this.context.getAttribute("system.directory") != null ? this.context.getAttribute("system.directory").toString() + "/files" : "files";
-
-		Builders builders = new Builders();
-		try {
-			MultipartFormData iter = new MultipartFormData(request);
-			ContentDisposition e = null;
-			while ((e = iter.getNextPart()) != null) {
-				final String fileName = e.getFileName();
-				final Builder builder = new Builder();
-				builder.put("type", StringUtilities.implode(";", Arrays.asList(e.getContentType())));
-				builder.put("file", new StringBuffer().append(this.context.getAttribute("HTTP_SCHEME")).append("://").append(this.context.getAttribute("HTTP_SERVER")).append(":"+ request.getServerPort()).append( "/files/").append(fileName));
-        File f = new File(path + File.separator + fileName);  
-        if (!f.exists()) {
-          if (!f.getParentFile().exists()) {  
-            f.getParentFile().mkdirs();  
+          Builder builder = this.list.poll();
+          if (builder == null) {
+            builder = new Builder();
+          } else {
+            builder.remove("cmd");
           }
-        }  
 
-				OutputStream out = new FileOutputStream(f);
-				InputStream is = new ByteArrayInputStream(e.getData());
-				int read = 0;
-				final byte[] bytes = new byte[1024];
-				while ((read = is.read(bytes)) != -1) {
-					out.write(bytes, 0, read);
-				}
-				out.close();
+          builder.put("user", request.getSession(true).getAttribute("user"));
+          builder.put("time", format.format(new Date()));
+          builder.put("message", filter(request.getParameter("text")));
 
-				builders.add(builder);
-				System.out.println("New file " + fileName + " created at " + path);
-				System.out.println(String.format("File %s being uploaded to %s", new Object[] { fileName, path }));
-			}
+          this.list.add(builder);
+          this.list.notifyAll();
+          return true;
+        }
+      }
+    }
 
-		} catch (IOException e) {
-			throw new ApplicationException(e.getMessage(), e);
-		} catch (ServletException e) {
-			throw new ApplicationException(e.getMessage(), e);
-		}
+    return false;
+  }
 
-		return builders.toString();
-	}
+  public String command() {
+    HttpServletRequest request = (HttpServletRequest) this.context
+        .getAttribute("HTTP_REQUEST");
+    HttpServletResponse response = (HttpServletResponse) this.context
+        .getAttribute("HTTP_RESPONSE");
 
-	private String filter(String text) {
-		text = text.replaceAll("<script(.*)>(.*)<\\/script>", "");
-		return text;
-	}
+    if (request.getSession().getAttribute("meeting_code") != null) {
+      response.setContentType("application/json");
+      if (request.getSession().getAttribute("user") == null) {
+        return "{ \"error\": \"missing user\" }";
+      }
 
-	@Override
-	public String version() {
-		return "Welcome to use tinystruct 2.0";
-	}
+      this.checkup(request);
+      synchronized (this.list) {
+        Builder builder = this.list.poll();
+        if (builder == null) {
+          builder = new Builder();
+        } else {
+          builder.remove("message");
+          builder.remove("time");
+        }
+
+        builder.put("user", request.getSession(true).getAttribute("user"));
+        builder.put("cmd", request.getParameter("cmd"));
+
+        this.list.add(builder);
+        this.list.notifyAll();
+        return "{}";
+      }
+    }
+
+    return "{ \"error\": \"expired\" }";
+  }
+  
+  public String upload() throws ApplicationException {
+    HttpServletRequest request = (HttpServletRequest) this.context.getAttribute("HTTP_REQUEST");
+    HttpServletResponse response = (HttpServletResponse) this.context.getAttribute("HTTP_RESPONSE");
+    response.setContentType("text/html;charset=UTF-8");
+
+    // Create path components to save the file
+    final String path = this.context.getAttribute("system.directory") != null ? this.context.getAttribute("system.directory").toString() + "/files" : "files";
+
+    Builders builders = new Builders();
+    try {
+      MultipartFormData iter = new MultipartFormData(request);
+      ContentDisposition e = null;
+      int read = 0;
+      while ((e = iter.getNextPart()) != null) {
+        final String fileName = e.getFileName();
+        final Builder builder = new Builder();
+        builder.put("type", StringUtilities.implode(";", Arrays.asList(e.getContentType())));
+        builder.put("file", new StringBuffer().append(this.context.getAttribute("HTTP_SCHEME")).append("://").append(this.context.getAttribute("HTTP_SERVER")).append(":"+ request.getServerPort()).append( "/files/").append(fileName));
+        File f = new File(path + File.separator + fileName);
+        if (!f.exists()) {
+          if (!f.getParentFile().exists()) {
+            f.getParentFile().mkdirs();
+          }
+        }
+
+        OutputStream out = new FileOutputStream(f);
+        BufferedOutputStream bout= new BufferedOutputStream(out);
+        ByteArrayInputStream is = new ByteArrayInputStream(e.getData());
+        BufferedInputStream bs = new BufferedInputStream(is);
+        final byte[] bytes = new byte[8192];
+        while ((read = bs.read(bytes)) != -1) {
+           bout.write(bytes, 0, read);
+        }
+        bout.close();
+        bs.close();
+
+        builders.add(builder);
+        System.out.println("New file " + fileName + " created at " + path);
+        System.out.println(String.format("File %s being uploaded to %s", new Object[] { fileName, path }));
+      }
+
+    } catch (IOException e) {
+      throw new ApplicationException(e.getMessage(), e);
+    } catch (ServletException e) {
+      throw new ApplicationException(e.getMessage(), e);
+    }
+
+    return builders.toString();
+  }
+
+  public boolean topic() {
+    HttpServletRequest request = (HttpServletRequest) this.context.getAttribute("HTTP_REQUEST");
+    if (request.getSession().getAttribute("meeting_code") != null) {
+      String key = request.getSession().getAttribute("meeting_code").toString();
+      this.setVariable(key, filter(request.getParameter("topic")));
+      return true;
+    }
+
+    return false;
+  }
+
+  public smalltalk exit(String meeting_code) {
+    HttpServletRequest request = (HttpServletRequest) this.context.getAttribute("HTTP_REQUEST");
+    request.getSession().removeAttribute("meeting_code");
+
+    return this;
+  }
+
+  private void checkup(HttpServletRequest request) {
+    String key = request.getSession().getAttribute("meeting_code").toString();
+    if ((this.list = map.get(key)) == null) {
+      this.list = new ConcurrentLinkedQueue<Builder>();
+      this.map.put(key, this.list);
+      this.setVariable("meeting_code", key);
+    }
+  }
+
+  private String filter(String text) {
+    text = text.replaceAll("<script(.*)>(.*)<\\/script>", "");
+    return text;
+  }
+
+  @Override
+  public String version() {
+    return "Welcome to use tinystruct 2.0";
+  }
 }
