95572e5b486a1004fb192cb79f51bd3296127bcd
James <moverinfo@gmail.com>
2020-05-24 23:19:00 +0800

    Update RequestMerger.java

--- src/main/java/tinystruct/examples/RequestMerger.java
+++ src/main/java/tinystruct/examples/RequestMerger.java
@@ -1,12 +1,71 @@
 package tinystruct.examples;
 
-public class a {
-    private static a ourInstance = new a();
+import org.tinystruct.AbstractApplication;
 
-    public static a getInstance() {
+import java.util.HashMap;
+import java.util.concurrent.*;
+
+@SuppressWarnings("unchecked")
+public class RequestMerger extends AbstractApplication {
+    private static RequestMerger ourInstance = new RequestMerger();
+    private LinkedBlockingQueue<AsyncRequest> queue = new LinkedBlockingQueue<AsyncRequest>();
+    private ScheduledThreadPoolExecutor executorService;
+
+    public static RequestMerger getInstance() {
         return ourInstance;
     }
 
-    private a() {
+    private RequestMerger() {
     }
+
+    @Override
+    public void init() {
+        executorService = new ScheduledThreadPoolExecutor(1);
+        executorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                int size = queue.size();
+                for(int i = 0 ; i < size; i++) {
+                    AsyncRequest req = queue.poll();
+                }
+
+
+
+/*
+                HashMap<String, Map<String,Result>> responses = new HashMap();
+                for(Map response : responses) {
+                    String requestId = response.get("requestId").toString();
+                    responses.put(requestId, response);
+                }
+*/
+                
+
+            }
+        }, 0, 10, TimeUnit.MILLISECONDS);
+    }
+
+    public HashMap<String, Result> take(AsyncRequest request) throws InterruptedException, ExecutionException {
+        request.future = new CompletableFuture();
+        queue.put(request);
+
+        return request.future.get();
+    }
+
+    private void workon(AsyncRequest request) {
+
+    }
+
+    @Override
+    public String version() {
+        return null;
+    }
+}
+
+class AsyncRequest {
+    public String requestId;
+    public Future<HashMap<String, Result>> future;
+}
+
+interface Result<T> {
+    T get();
 }
