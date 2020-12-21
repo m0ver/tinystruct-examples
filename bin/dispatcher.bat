ed0a9f8e341366f39f5bf131350f59e70e803e79
James <moverinfo@gmail.com>
2020-12-22 00:18:33 +0800

    Update dispatcher.

--- bin/dispatcher.bat
+++ bin/dispatcher.bat
@@ -1,4 +1,4 @@
-@echo off
+src/main/java/org/tinystruct/system/ApplicationManager.java@echo off
 set "ROOT=%~dp0../"
 set "classpath=%ROOT%lib\*:%ROOT%WEB-INF\lib\*:%ROOT%WEB-INF\classes":%classpath%
 @java -cp "%ROOT%lib\*;%ROOT%WEB-INF\lib\*;%ROOT%WEB-INF\classes" org.tinystruct.system.Dispatcher %*
